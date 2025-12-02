56f7859b675cbe8f2826ce27abd80968dfcba679
swaptox <xinglai750@gmail.com>
2025-12-03 00:58:26 +0800

    Uncomment supabase client export

--- src/_database.ts
+++ src/_database.ts
@@ -1,7 +1,2 @@
 import { createClient } from 'https://esm.sh/@supabase/supabase-js@2';
-//import process from "node:process";
-
-console.log('=========url----------', Deno.env.get('SUPABASE_URL'));
-console.log('=========key----------', Deno.env.get('SUPABASE_ANON_KEY'));
-
 export const supabase = createClient(Deno.env.get('SUPABASE_URL'), Deno.env.get('SUPABASE_ANON_KEY'));
