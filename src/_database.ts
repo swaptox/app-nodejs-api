b3d923e3e6f177a7871b38f9eac72905b275ac30
swaptox <xinglai750@gmail.com>
2025-12-03 00:42:45 +0800

    Log Supabase URL and key on initialization
    Add logging for Supabase URL and key

--- src/_database.ts
+++ src/_database.ts
@@ -1,3 +1,7 @@
 import { createClient } from 'https://esm.sh/@supabase/supabase-js@2';
 import process from "node:process";
+
+console.log('=========url', process.env.SUPABASE_URL);
+console.log('=========key', process.env.SUPABASE_ANON_KEY);
+
 export const supabase = createClient(process.env.SUPABASE_URL, process.env.SUPABASE_ANON_KEY);
