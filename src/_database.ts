7779ca32f96935d979f1d78895317adb1afca2f4
swaptox <xinglai750@gmail.com>
2025-12-03 03:12:26 +0800

    Update Supabase client version to 2.8.0

--- src/_database.ts
+++ src/_database.ts
@@ -1,2 +1,2 @@
-import { createClient } from 'https://esm.sh/@supabase/supabase-js@2';
+import { createClient } from 'https://esm.sh/@supabase/supabase-js@2.8.0';
 export const supabase = createClient(Deno.env.get('SUPABASE_URL'), Deno.env.get('SUPABASE_ANON_KEY'));
