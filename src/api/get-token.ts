691aada0ccbf133b8481affd10d5770916bc0478
swaptox <xinglai750@gmail.com>
2025-12-02 23:55:38 +0800

    Enhance getToken with pagination and search
    Added pagination and keyword search functionality to getToken.

--- src/api/get-token.ts
+++ src/api/get-token.ts
@@ -1,16 +1,35 @@
 import { corsHeaders } from '../_cors.ts';
 import { supabase } from '../_database.ts';
 export const getToken = async (chain, req) => {
-	//const url = new URL(req.url);
-	//const searchParams = url.searchParams;
-
-	return new Response(JSON.stringify(req), {
+	const url = new URL(req.url);
+	const searchParams = url.searchParams;
+	const page = parseInt(searchParams.get('page') || '1');
+	const pageSize = parseInt(searchParams.get('size') || '100');
+	const from = (page - 1) * pageSize;
+	const to = from + pageSize - 1;
+	let query = supabase.from(`${chain}-token`).select('decimals, name, symbol, address, logo, has_permit, price_usd');
+	let keyword = searchParams.get('keyword');
+	if (keyword) {
+		query = query.or(`name.ilike.%${keyword}%,symbol.ilike.%${keyword}%,address.ilike.%${keyword}%`); // 搜索
+	}
+	query = query.eq('disabled', false);
+	let onlyVerified = searchParams.get('onlyVerified')?.toString();
+	if (onlyVerified === 'true') {
+		query = query.eq('is_verified', true); // *新增筛选条件*
+	}
+	const { data, error } = await query.order('sorting', {
+		ascending: false // 根据 sorting 降序排列
+	}).order('id', {
+		ascending: true
+	}).range(from, to);
+	if (error) {
+		throw error;
+	}
+	return new Response(JSON.stringify(data), {
 		headers: {
 			...corsHeaders,
 			'Content-Type': 'application/json'
 		},
 		status: 200
 	});
-	
-	
 };
