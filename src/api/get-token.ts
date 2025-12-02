40bec85cd42d4b71ab16fda968d59ea8eb785e3e
swaptox <xinglai750@gmail.com>
2025-12-02 23:48:39 +0800

    Change response to return req instead of url

--- src/api/get-token.ts
+++ src/api/get-token.ts
@@ -1,10 +1,10 @@
 import { corsHeaders } from '../_cors.ts';
 import { supabase } from '../_database.ts';
 export const getToken = async (chain, req) => {
-	const url = new URL(req.url);
+	//const url = new URL(req.url);
 	//const searchParams = url.searchParams;
 
-	return new Response(JSON.stringify(url), {
+	return new Response(JSON.stringify(req), {
 		headers: {
 			...corsHeaders,
 			'Content-Type': 'application/json'
