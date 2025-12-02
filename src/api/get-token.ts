62bd9c7cb322726a02eed7c9359e5678d18df8d6
swaptox <xinglai750@gmail.com>
2025-12-02 23:25:18 +0800

    Implement getToken function to handle requests

---
+++ src/api/get-token.ts
@@ -0,0 +1,16 @@
+import { corsHeaders } from '../_cors.ts';
+import { supabase } from '../_database.ts';
+export const getToken = async (chain, req) => {
+	const url = new URL(req.url);
+	const searchParams = url.searchParams;
+
+	return new Response(JSON.stringify(searchParams), {
+		headers: {
+			...corsHeaders,
+			'Content-Type': 'application/json'
+		},
+		status: 200
+	});
+	
+	
+};
