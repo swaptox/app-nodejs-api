f4e0373c5ee3afc660aa853097dac6ed4e4424cb
swaptox <xinglai750@gmail.com>
2025-12-03 03:46:38 +0800

    Add dynamic RPC URL selection to config

--- src/_config.ts
+++ src/_config.ts
@@ -1,10 +1,22 @@
 
+const baseRpcUrls = [];
+if(Deno.env.get('BASE_RPC_1')){baseRpcUrls.push(Deno.env.get('BASE_RPC_1'));}
+if(Deno.env.get('BASE_RPC_2')){baseRpcUrls.push(Deno.env.get('BASE_RPC_2'));}
+if(Deno.env.get('BASE_RPC_3')){baseRpcUrls.push(Deno.env.get('BASE_RPC_3'));}
+if(Deno.env.get('BASE_RPC_4')){baseRpcUrls.push(Deno.env.get('BASE_RPC_4'));}
+if(Deno.env.get('BASE_RPC_5')){baseRpcUrls.push(Deno.env.get('BASE_RPC_5'));}
+const randomUrl = baseRpcUrls[Math.floor(Math.random() * baseRpcUrls.length)];
+
+
+
+
+
 
 
 export const config = {
 	'base': {
 		'routeAddress': '0x044C6cF8994aED67fd7B95aE1D9b714724eF6a6c',
-		'rpc': [],
+		'rpc': randomUrl,
 		'ether': {
 			'native': '0xeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee',
 			'wrapped': '0x4200000000000000000000000000000000000006',
